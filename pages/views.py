cc893063502a21843e2e220b97b1edffef1e0cfa
Pranjo Sudjman <dicroatiano@gmail.com>
2019-11-16 18:30:20 +0100

    everything up to auth

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from listings.choices import state_choices, bedroom_choices, price_choices
+from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    contex = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'pages/index.html', contex)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors =  Realtor.objects.all().filter(is_mvp=True)
+    contex = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', contex)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'name', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 10
+
+admin.site.register(Realtor, RealtorAdmin)
